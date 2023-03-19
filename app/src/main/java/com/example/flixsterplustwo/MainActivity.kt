b9119ac577035cd1fe0e31f7c3f59fbb64609e4a
Melissa Perez <melmperz@gmail.com>
2023-03-19 16:26:11 -0400

    Update more settings

--- app/src/main/java/com/example/flixsterplustwo/MainActivity.kt
+++ app/src/main/java/com/example/flixsterplustwo/MainActivity.kt
@@ -3,9 +3,15 @@ package com.example.flixsterplustwo
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+private const val TAG = "MainActivity/"
+private const val SEARCH_API_KEY = BuildConfig.API_KEY
+private const val TOP_RATED_MOVIES_URL =
+    "https://api.themoviedb.org/3/movie/top_rated"
+
+
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.id.layout.activity_main)
     }
 }
